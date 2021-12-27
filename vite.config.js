86ed98b5ddf0358095fd8317bacb6f95255d2857
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2021-12-27 23:21:51 +0700

    Fix wrong build form production

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
-import compress from 'vite-plugin-compress'
-import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue(), crossOriginIsolation(), compress(), viteCompression()]
+	plugins: [vue(), crossOriginIsolation()]
 })
