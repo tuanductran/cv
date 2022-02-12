3d9aea75b3b569332e3ae6161c1dea631d7b4e86
Tuan Duc Design <tuanducdesign@Tuans-MacBook-Air.local>
2022-02-12 14:08:39 +0700

    Fix ga

--- src/app.js
+++ src/app.js
@@ -1,14 +1,9 @@
 import { createApp } from 'vue'
-import galite from 'ga-lite';
-
-galite('create', 'UA-175649416-2', 'auto')
-galite('send', 'pageview')
 import App from './App.vue'
 
 import SectionContent from './components/SectionContent/index.vue'
 
 import infos from '/public/data/infos.json'
-
 const app = createApp(App)
 
 // injeet the data in the app
