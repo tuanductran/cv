d728931ca62e602a86ee4f8a350e06c537dc479f
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-02-28 01:25:45 +0700

    Remove Footer

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup>
    import AppContainer from './layouts/AppContainer/index.vue'
    import AppHeader from './layouts/AppHeader/index.vue'
-   import AppFooter from './layouts/AppFooter/index.vue'
 </script>
 
 <template>
    <app-header />
    <app-container />
-   <app-footer />
 </template>
 
 <style lang="scss">
