457912dc60c908893b4d8c2f4b31e5e530107735
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-03-01 09:51:35 +0700

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import ViteRadar from 'vite-plugin-radar'
+import { viteSingleFile } from "vite-plugin-singlefile"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,31 @@ export default defineConfig({
 			// Google Analytics tag injection
 			analytics: {
 			  id: 'UA-175649416-2',
+			  config: {
+				cookie_domain: 'auto',
+				cookie_expires: 63072000,
+				cookie_prefix: 'none',
+				cookie_update: true,
+				cookie_flags: '',
+				send_page_view: true,
+				allow_google_signals: true,
+				allow_ad_personalization_signals: true,
+			  },
 			},
-		  })
+		  }),
+		viteSingleFile()
 		],
+		build: {
+			target: "esnext",
+			assetsInlineLimit: 100000000,
+			chunkSizeWarningLimit: 100000000,
+			cssCodeSplit: false,
+			brotliSize: false,
+			rollupOptions: {
+				inlineDynamicImports: true,
+				output: {
+					manualChunks: () => "bundle.js",
+				},
+			},
+		},
 })
