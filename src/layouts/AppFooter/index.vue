68601f387b4b9a8eb66714aac8c002b6defde701
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-06-13 06:07:54 +0700

    Update copyright

--- src/layouts/AppFooter/index.vue
+++ src/layouts/AppFooter/index.vue
@@ -5,7 +5,7 @@
 <template>
 	<footer class="flex justify-center relative py-5 bg-white dark:bg-footer-dark bg-opacity-70 mt-20">
 			<p class="text-color-body dark:text-white text-sm mt-5 mb-5">
-				Made with <i class="fa fa-heart"></i> <span class="dot">·</span> Powered by <strong><a href="https://tuanducdesign.com" target="_blank" title="Tuan Duc Design">Tuan Duc Design</a></strong>
+				Được tạo bằng cả <i class="fa fa-heart"></i> <span class="dot">·</span> Xây dựng và bảo trì bởi <strong><a href="https://github.com/vnodesign" target="_blank" title="VNO Design">VNO Design</a></strong>
 			</p>
 	</footer>
 </template>
