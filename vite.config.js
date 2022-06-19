d07b4e3a2bf9deffcd484e80bb2482f52fc6ed9a
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-06-19 20:30:27 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { viteSingleFile } from "vite-plugin-singlefile"
+ import { defineConfig } from 'vite'
+ import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [
-		vue(),
-		viteSingleFile()
-		],
-		build: {
-			target: "esnext",
-			assetsInlineLimit: 100000000,
-			chunkSizeWarningLimit: 100000000,
-			cssCodeSplit: true,
-			brotliSize: true,
-			rollupOptions: {
-				inlineDynamicImports: true,
-				output: {
-					manualChunks: () => "bundle.js",
-				},
-			},
-		},
-})
+ // https://vitejs.dev/config/
+ export default defineConfig({
+ 	plugins: [vue()]
+ })
