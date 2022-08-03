f362eb10ba8d537e8f18b6f351f038a8fdb8ecc9
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-08-03 13:51:22 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <script setup>
-   import AppContainer from './layouts/AppContainer/index.vue'
+   // import AppContainer from './layouts/AppContainer/index.vue'
    import AppHeader from './layouts/AppHeader/index.vue'
-   import AppFooter from './layouts/AppFooter/index.vue'
+   // import AppFooter from './layouts/AppFooter/index.vue'
 </script>
 
 <template>
-   <app-header />
    <app-container />
-   <app-footer />
 </template>
 
 <style lang="scss">
    @import 'assets/styles/styles.css';
    html {
-      scroll-behavior: smooth;
       .center-element {
          @apply flex justify-center items-center;
       };
