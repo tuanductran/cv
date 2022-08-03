b7327d1b147b3142f0ef76895093e27cc2f9997d
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-08-03 18:39:04 +0700

    Update index.vue

--- src/views/CoverLetter/index.vue
+++ src/views/CoverLetter/index.vue
@@ -9,7 +9,7 @@
 </script>
 
 <template>
-   <section id="coverletter" class="mb-20">
+   <section id="coverletter">
       <section-content title="Thư xin việc">
          <div>
             <!-- Cover Letter information -->
