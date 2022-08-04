4031375e18f88f2f76a2c620dc51291d16f2220b
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-08-05 05:28:18 +0700

    Fix dark mode

--- src/views/Contact/index.vue
+++ src/views/Contact/index.vue
@@ -12,19 +12,19 @@
            <label class="label">
              <span class="label-text">Địa chỉ Email:</span>
            </label>
-           <input type="email" name="email" placeholder="Địa chỉ Email" class="input input-bordered bg-contact-dark w-full" required />
+           <input type="email" name="email" placeholder="Địa chỉ Email" class="input input-bordered dark:bg-contact-dark w-full" required />
          </div>
          <div class="form-control w-full">
            <label class="label">
              <span class="label-text">Họ và tên:</span>
            </label>
-           <input type="fullname" name="fullname" placeholder="Họ và tên" class="input input-bordered bg-contact-dark w-full" required />
+           <input type="fullname" name="fullname" placeholder="Họ và tên" class="input input-bordered dark:bg-contact-dark w-full" required />
          </div>
          <div class="form-control w-full">
            <label class="label">
              <span class="label-text">Nội dung gửi đi:</span>
            </label>
-           <textarea class="textarea textarea-bordered h-24" placeholder="Nhập nội dung mà bạn muốn gửi đến tôi ở đây" required></textarea>
+           <textarea class="textarea textarea-bordered dark:bg-contact-dark h-24" placeholder="Nhập nội dung mà bạn muốn gửi đến tôi ở đây" required></textarea>
          </div>
          <button id="submit_job" type="submit" class="btn btn-outline my-5">Send</button>
          <p id="send__job__status" class="text-main-color"></p>
