6b2eb76353789255c0ac1c04cbff6aedaa820ab8
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-08-05 02:26:58 +0700

    Update index.vue

--- src/views/Projects/ProjectDetails/index.vue
+++ src/views/Projects/ProjectDetails/index.vue
@@ -5,33 +5,20 @@
 </script>
 
 <template>
-   <figcaption class="mt-3 md:mt-0 z-10">
-      <!-- Project title -->
-      <h2 class="text-2xl xl:text-4xl 2xl:text-2xl font-bold mb-3 xl:mb-6 2xl:mb-3">
-         {{ project.title }}
-      </h2>
-      <!-- End Project title -->
-      <p>
-         <a class="text-xs lg:text-base text-main-color" v-for="link, k in project.links" :href="link" :key="k" target="_blank" onClick="ga('event', 'ProjectLink', 'view', 'project_link');">
-            <strong>{{ link }}</strong>
-         </a>
-      </p>
-      <!-- Project descript -->
-      <div class="rounded-lg mb-3 bg-white dark:bg-product-dark p-5 mt-5 vnodesign-border dark:vnodesign-border">
-         <p class="leading-relaxed text-left text-color-body dark:text-white text-sm xl:text-base"  v-for="description, k in project.description" :key="`description-${ k }`">
-            {{ description }}
-         </p>
+   <div class="card-body">
+    <h2 class="card-title">{{ project.title }}</h2>
+    <p>
+       <a class="text-xs lg:text-base text-main-color" v-for="link, k in project.links" :href="link" :key="k" target="_blank" onClick="ga('event', 'ProjectLink', 'view', 'project_link');">
+           <strong>{{ link }}</strong>
+       </a>
+    </p>
+    <p class="leading-relaxed text-left text-color-body dark:text-white text-sm xl:text-base" v-for="description, k in project.description" :key="`description-${ k }`">
+          {{ description }}
+    </p>
+    <div class="card-actions justify-end">
+      <div class="badge badge-outline" v-for="tech, k in project.builtWidth" :key="k">
+          {{ tech }}
       </div>
-      <!-- End Project descript -->
-
-      <!-- Project technologies -->
-      <ul class="flex mt-5">
-         <li
-            class="text-xs lg:text-base text-main-color rounded-full mr-7 last:mr-0"
-            v-for="tech, k in project.builtWidth"
-            :key="k"
-         ><strong>{{ tech }}</strong></li>
-      </ul>
-      <!-- End Project technologies -->
-   </figcaption>
+    </div>
+  </div>
 </template>
