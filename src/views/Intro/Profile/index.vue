5ac975df72ba52efc635098dc74e083ffd0ba2b1
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-08-04 15:07:42 +0700

    Fix text

--- src/views/Intro/Profile/index.vue
+++ src/views/Intro/Profile/index.vue
@@ -28,7 +28,7 @@
          <!-- Github -->
          <a class="text-sm border border-main-color w-max block mt-3 py-2 px-3 text-main-color rounded-full" role="button" :href="intro.links[projectLinks]" title="Xem thêm trên Github" target="_blank" onClick="ga('event', 'ProjectsLink', 'view', 'projects_link');">
             <i :class="`fa fa-${ projectLinks } mr-2`"></i>
-            <span>xem thêm trên Github</span>
+            <span>Xem thêm trên Github</span>
          </a>
          <!-- End Github -->
       </div>
