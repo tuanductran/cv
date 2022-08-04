aaefc8081c59895e9b77eb1b3dd17898cd28f6c2
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-08-05 02:16:14 +0700

    Update index.vue

--- src/views/Projects/index.vue
+++ src/views/Projects/index.vue
@@ -11,14 +11,12 @@
    <section id="projects">
       <section-content title="Sản phẩm">
          <div class="xl:grid gap-10 2xl:grid-cols-3">
-            <article class="mb-10 md:mb-10 xl:mb-16 2xl:mb-5 last:mb-0" v-for="project, k in projects" :key="k">
-               <figure class="grid md:grid-cols-2 2xl:grid-cols-1 md:gap-4 lg:gap-7">
-                  <!-- Project thumbnail -->
-                  <project-thumbnail :project="project"/>
-                  <!-- Project details -->
-                  <project-details :project="project"/>
-               </figure>
-            </article>
+           <div class="card w-full bg-white dark:bg-product">
+              <figure>
+                <project-thumbnail :project="project"/>
+              </figure>
+              <project-details :project="project"/>
+           </div>
          </div>
       </section-content>
    </section>
