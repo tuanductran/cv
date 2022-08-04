6eb52a0760aa05320d095550ab72a256abba856c
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-08-04 21:00:54 +0700

    Update index.vue

--- src/components/SectionContent/index.vue
+++ src/components/SectionContent/index.vue
@@ -5,7 +5,7 @@
 </script>
 
 <template>
-   <div class="flex py-10 md:py-14 lg:py-16 mt-7">
+   <div class="flex py-10 md:py-14 lg:py-16 mt-3">
       <!-- Section title -->
       <h1 class="whitespace-nowrap mx-3 uppercase font-bold text-xl md:text-2xl xl:text-3xl">
          {{ title }}
