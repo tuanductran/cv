96ffb1ae9fbbd60958161c6011bb97f151e33e71
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-08-05 02:35:27 +0700

    Update index.vue

--- src/views/Projects/index.vue
+++ src/views/Projects/index.vue
@@ -12,10 +12,8 @@
       <section-content title="Sản phẩm">
          <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
            <div class="card w-100 bg-white dark:bg-product">
-              <figure>
                 <project-thumbnail :project="project"/>
-              </figure>
-              <project-details :project="project"/>
+                <project-details :project="project"/>
            </div>
          </div>
       </section-content>
