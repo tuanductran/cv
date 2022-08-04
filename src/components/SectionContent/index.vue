56ad033b4cbf9e35ed53b3b65a7723cfc88e82c0
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-08-04 14:28:36 +0700

    Update index.vue
    Change center to left text

--- src/components/SectionContent/index.vue
+++ src/components/SectionContent/index.vue
@@ -5,18 +5,12 @@
 </script>
 
 <template>
-   <div class="flex items-center justify-center py-10 md:py-14 lg:py-16 mt-12">
-      <!-- Left bar -->
-      <div class="block w-1/2 h-px bg-gray-300 dark:bg-title-dark"></div>
-      <!-- End Left bar -->
+   <div class="flex py-10 md:py-14 lg:py-16 mt-12">
       <!-- Section title -->
       <h1 class="whitespace-nowrap mx-3 uppercase font-bold text-xl md:text-2xl xl:text-3xl">
          {{ title }}
       </h1>
       <!-- End Section title -->
-      <!-- Right bar -->
-      <div class="block w-1/2 h-px bg-gray-300 dark:bg-title-dark"></div>
-      <!-- End Right bar -->
    </div>
    <!-- Section content -->
    <slot />
