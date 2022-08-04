23cc0324bec216d65a007b8c8a223fe7bd80073c
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-08-05 00:03:04 +0700

    Update index.vue

--- src/components/SectionContent/index.vue
+++ src/components/SectionContent/index.vue
@@ -5,12 +5,18 @@
 </script>
 
 <template>
-   <div class="flex my-7">
+   <div class="flex items-center justify-center my-7">
+      <!-- Left bar -->
+      <div class="block w-1/2 h-px bg-gray-300 dark:bg-title-dark"></div>
+      <!-- End Left bar -->
       <!-- Section title -->
       <h1 class="whitespace-nowrap mx-3 uppercase font-bold text-xl md:text-2xl xl:text-3xl">
          {{ title }}
       </h1>
       <!-- End Section title -->
+      <!-- Right bar -->
+      <div class="block w-1/2 h-px bg-gray-300 dark:bg-title-dark"></div>
+      <!-- End Right bar -->
    </div>
    <!-- Section content -->
    <slot />
