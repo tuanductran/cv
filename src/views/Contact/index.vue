9eaa2440fa12580ec4468520075400af3ef17421
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-08-05 09:50:29 +0700

    Update index.vue

--- src/views/Contact/index.vue
+++ src/views/Contact/index.vue
@@ -7,7 +7,7 @@
 <template>
    <section id="contact">
    <section-content title="Liên hệ">
-      <form id="send_job" action="https://formspree.io/f/mknkawry" method="POST">
+      <form name="send_job" id="send_job" action="https://formspree.io/f/mknkawry" method="POST">
          <div class="form-control w-full">
            <label class="label">
              <span class="label-text">Địa chỉ Email:</span>
@@ -18,13 +18,13 @@
            <label class="label">
              <span class="label-text">Họ và tên:</span>
            </label>
-           <input type="fullname" name="fullname" placeholder="Họ và tên" class="input input-bordered dark:bg-contact-dark w-full" required />
+           <input type="name" name="fullname" placeholder="Họ và tên" class="input input-bordered dark:bg-contact-dark w-full" required />
          </div>
          <div class="form-control w-full">
            <label class="label">
              <span class="label-text">Nội dung gửi đi:</span>
            </label>
-           <textarea class="textarea textarea-bordered dark:bg-contact-dark h-24" placeholder="Nhập nội dung mà bạn muốn gửi đến tôi ở đây" required></textarea>
+           <textarea name="content_job" class="textarea textarea-bordered dark:bg-contact-dark h-24" placeholder="Nhập nội dung mà bạn muốn gửi đến tôi ở đây" required />
          </div>
          <button id="submit_job" type="submit" class="btn btn-outline my-5">Send</button>
          <p id="send__job__status" class="text-main-color"></p>
