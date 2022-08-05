718e7a6bbfa56da0dd2a7beb11eb505a3f6de7c0
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-08-05 13:57:08 +0700

    Update index.vue

--- src/views/Intro/Profile/index.vue
+++ src/views/Intro/Profile/index.vue
@@ -26,13 +26,13 @@
          </p>
          <!-- End Role -->
          <!-- Github -->
-         <a class="badge bg-transparent border-main-color w-max mt-3 py-2 px-3 text-main-color" role="button" :href="intro.links[projectLinks]" title="Xem thêm trên Github" target="_blank" onClick="ga('event', 'ProjectsLink', 'view', 'projects_link');">
+         <a class="badge bg-transparent border-main-color w-max mt-3 py-2 mx-3 px-3 text-main-color" role="button" :href="intro.links[projectLinks]" title="Xem thêm trên Github" target="_blank" onClick="ga('event', 'ProjectsLink', 'view', 'projects_link');">
             <i :class="`fa fa-${ projectLinks } mr-2`" />
             <span>Xem thêm trên Github</span>
          </a>
          <!-- End Github -->
          <!-- Coverletter -->
-         <a class="badge bg-transparent border-main-color w-max mt-3 py-2 px-3 text-main-color" role="button" href="#view_coverletter" title="Xem thư xin việc" onClick="ga('event', 'CoverletterPopup', 'view', 'coverletter_popup');">
+         <a class="badge bg-transparent border-main-color w-max mt-3 py-2 mx-3 px-3 text-main-color" role="button" id="show__modal" href="#view_coverletter" title="Xem thư xin việc" onClick="ga('event', 'CoverletterPopup', 'view', 'coverletter_popup');">
             <i class="fa fa-pencil mr-2" />
             <span>Xem thư xin việc</span>
          </a>
