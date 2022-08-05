c86ff708d25e122c778bdfd271151c636e0992a8
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-08-06 00:09:54 +0700

    Update index.vue

--- src/views/Projects/ProjectDetails/index.vue
+++ src/views/Projects/ProjectDetails/index.vue
@@ -6,7 +6,10 @@
 
 <template>
    <div class="card-body">
-    <h2 class="card-title">{{ project.title }}</h2>
+    <h2 class="card-title">
+       {{ project.title }}
+       <a class="badge badge-outline p-3" href="`{{ project.sourceCodeUrl }}`" target="_blank" onClick="ga('event', 'SourceCodeUrl', 'view', 'source_code_url');">Source code</a>
+    </h2>
     <p>
        <a class="text-xs lg:text-base text-main-color" v-for="link, k in project.links" :href="link" :key="k" target="_blank" onClick="ga('event', 'ProjectLink', 'view', 'project_link');">
            <strong>{{ link }}</strong>
