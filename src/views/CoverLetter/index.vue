c1ecf09f3f5f814902f9fbdb1e2ade8132408ad1
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-08-05 15:15:37 +0700

    Update index.vue

--- src/views/CoverLetter/index.vue
+++ src/views/CoverLetter/index.vue
@@ -9,12 +9,14 @@
 </script>
 
 <template>
-<div class="modal" id="view_coverletter">
+<div class="modal modal-bottom sm:modal-middle" id="view_coverletter">
   <div class="modal-box">
     <h3 class="font-bold text-lg">Thư xin việc</h3>
+    <div class="divider"></div>
     <p class="py-4 text-color-body dark:text-white"  v-for="content, k in coverletter.content" :key="`content-${ k }`">
       {{ content }}
     </p>
+    <div class="divider"></div>
     <div class="modal-action">
      <a href="#" class="btn" id="hide__modal">Đóng!</a>
     </div>
