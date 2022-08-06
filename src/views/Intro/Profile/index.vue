09367f89362c6b43177460eb3fb6ec5bdac1950f
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-08-06 14:45:56 +0700

    Update index.vue
    Recovery to default form branch main

--- src/views/Intro/Profile/index.vue
+++ src/views/Intro/Profile/index.vue
@@ -9,26 +9,22 @@
 <template>
    <div class="order-1">
       <!-- Personal infos -->
-      <div class="relative flex flex-col-reverse">
-         <div class="flex items-center justify-center space-x-4">
+      <div class="relative">
          <!-- Avatar -->
-            <img class="flex-none w-14 h-14 rounded-full object-cover" :alt="`${ intro.fullname } photo`" :src="`./assets/${ intro.avatar }`" loading="lazy" deasync="true">
+         <div class="cv-author mx-auto">
+            <img class="w-full rounded-full" :alt="`${ intro.fullname } photo`" :src="`./assets/${ intro.avatar }`" loading="lazy" deasync="true">
+         </div>
          <!-- End avatar -->
-         <div class="flex-auto">
          <!-- Fullname -->
-         <h1 class="font-semibold">
+         <h1 class="text-3xl lg:text-4xl xl:text-5xl font-bold mt-3">
             {{ intro.fullname }}
          </h1>
          <!-- End fullname -->
          <!-- Role -->
-         <p class="text-main-color font-semibold mt-0.5">
+         <p class="text-main-color font-semibold text-lg mt-3">
             {{ intro.role }}
          </p>
          <!-- End Role -->
-         </div>
-         </div>
-      </div>
-      <div class="relative">
          <!-- Github -->
          <a class="btn btn-sm md:btn-md btn-outline gap-2 normal-case lg:gap-3 mt-2" role="button" :href="intro.links[projectLinks]" title="Github Profile" target="_blank" onClick="ga('event', 'ProjectsLink', 'view', 'projects_link');">
              <i :class="`fa fa-${ projectLinks } mr-2`" />
@@ -43,3 +39,16 @@
       </div>
    </div>
 </template>
+<style lang="scss">
+.cv-author {
+   width: 100px;
+   height: 100px;
+   border-radius: 120px;
+   overflow: hidden;
+   display: inline-block;
+   border: 1px solid #ccc;
+   img {
+      width: 100%
+   }
+}
+</style>
