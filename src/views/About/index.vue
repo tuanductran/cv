1241c7d824527d2ed105c4637592b123843e2e95
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-08-07 00:08:05 +0700

    Update index.vue

--- src/views/About/index.vue
+++ src/views/About/index.vue
@@ -16,17 +16,17 @@
          <div class="grid grid-cols-1 lg:grid-cols-2 gap-5 my-10 md:my-14 lg:my-16">
             <!-- Personal information -->
             <div>
-               <h2 class="text-main-color font-bold mb-2">
+               <h2 class="text-main-body font-bold mb-2">
                   Mục tiêu nghề nghiệp
                </h2>
-               <p class="leading-relaxed mb-2 last:mb-0 text-color-body"  v-for="bio, k in about.bio" :key="`bio-${ k }`">
+               <p class="leading-relaxed mb-2 last:mb-0"  v-for="bio, k in about.bio" :key="`bio-${ k }`">
                   {{ bio }}
                </p>
             <!--
-               <h2 class="text-main-color font-bold mb-5">
+               <h2 class="text-main-body font-bold mb-5">
                   Thông tin cá nhân
                </h2>
-               <p class="leading-relaxed mb-4 last:mb-0 text-color-body"  v-for="personal_information, k in about.personal_information" :key="`personal_information-${ k }`">
+               <p class="leading-relaxed mb-4 last:mb-0"  v-for="personal_information, k in about.personal_information" :key="`personal_information-${ k }`">
                   {{ personal_information }}
                </p>
                <h2 class="text-main-color font-bold mb-5">
@@ -46,20 +46,20 @@
                   Thông tin liên hệ
                </h2>
                <!-- contacts info -->
-               <p class="leading-relaxed mb-2 last:mb-0 text-color-body" v-for="contactinfo_birthday, k in about.contactinfo_birthday" :key="`contactinfo_birthday-${ k }`">
+               <p class="leading-relaxed mb-2 last:mb-0" v-for="contactinfo_birthday, k in about.contactinfo_birthday" :key="`contactinfo_birthday-${ k }`">
                   <i class="fa fa-birthday-cake mr-2" /> {{ contactinfo_birthday }}
                </p>
-               <p class="leading-relaxed mb-2 last:mb-0 text-color-body" v-for="contactinfo_phone, k in about.contactinfo_phone" :key="`contactinfo_phone-${ k }`">
+               <p class="leading-relaxed mb-2 last:mb-0" v-for="contactinfo_phone, k in about.contactinfo_phone" :key="`contactinfo_phone-${ k }`">
                   <a :href="`tel:${ contactinfo_phone }`">
                     <i class="fa fa-phone mr-2" /> {{ contactinfo_phone }}
                   </a>
                </p>
-               <p class="leading-relaxed mb-2 last:mb-0 text-color-body" v-for="contactinfo_email, k in about.contactinfo_email" :key="`contactinfo_email-${ k }`">
+               <p class="leading-relaxed mb-2 last:mb-0" v-for="contactinfo_email, k in about.contactinfo_email" :key="`contactinfo_email-${ k }`">
                   <a :href="`mailto:${ contactinfo_email }`">
                     <i class="fa fa-envelope mr-2" /> {{ contactinfo_email }}
                   </a>
                </p>
-               <p class="leading-relaxed mb-2 last:mb-0 text-color-body" v-for="contactinfo_address, k in about.contactinfo_address" :key="`contactinfo_address-${ k }`">
+               <p class="leading-relaxed mb-2 last:mb-0" v-for="contactinfo_address, k in about.contactinfo_address" :key="`contactinfo_address-${ k }`">
                   <a :href="`https://www.google.com.vn/search?q=${ contactinfo_address }`" target="_blank">
                     <i class="fa fa-location-arrow mr-2" /> {{ contactinfo_address }}
                   </a>
@@ -84,7 +84,7 @@
                <h2 class="text-main-color font-bold mb-2">
                   Chứng chỉ
                </h2>
-               <p class="leading-relaxed mb-2 last:mb-0 text-color-body" v-for="certification, k in about.certification" :key="`certification-${ k }`">
+               <p class="leading-relaxed mb-2 last:mb-0" v-for="certification, k in about.certification" :key="`certification-${ k }`">
                   {{ certification }}
                </p>
             </div>
