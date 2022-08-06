31c08867782b321814ff7e1b1c2655997b0035bc
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-08-06 12:13:58 +0700

    Update index.vue

--- src/views/Intro/Profile/index.vue
+++ src/views/Intro/Profile/index.vue
@@ -26,13 +26,13 @@
          </p>
          <!-- End Role -->
          <!-- Github -->
-         <a class="badge bg-transparent border-main-color w-max mt-3 mx-1 py-2 px-3 text-main-color" role="button" :href="intro.links[projectLinks]" title="Github Project" target="_blank" onClick="ga('event', 'ProjectsLink', 'view', 'projects_link');">
+         <a class="btn btn-sm md:btn-md btn-ghost gap-2 normal-case lg:gap-3" role="button" :href="intro.links[projectLinks]" title="Github Project" target="_blank" onClick="ga('event', 'ProjectsLink', 'view', 'projects_link');">
             <i :class="`fa fa-${ projectLinks } mr-2`" />
             <span>Github Project</span>
          </a>
          <!-- End Github -->
          <!-- Coverletter -->
-         <a class="badge bg-transparent border-main-color w-max mt-3 mx-1 py-2 px-3 text-main-color" role="button" id="show__modal" href="#view_coverletter" title="Thư xin việc" onClick="ga('event', 'CoverletterPopup', 'view', 'coverletter_popup');">
+         <a class="btn btn-sm md:btn-md gap-2 normal-case lg:gap-3" role="button" id="show__modal" href="#view_coverletter" title="Thư xin việc" onClick="ga('event', 'CoverletterPopup', 'view', 'coverletter_popup');">
             <i class="fa fa-pencil mr-2" />
             <span>Thư xin việc</span>
          </a>
