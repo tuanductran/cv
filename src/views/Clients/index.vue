b0faca6f1969791651ec34166818b20bb272d585
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-08-06 23:55:14 +0700

    Update index.vue

--- src/views/Clients/index.vue
+++ src/views/Clients/index.vue
@@ -11,7 +11,7 @@
    <section id="clients">
       <section-content title="Khách Hàng">
          <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-           <div class="card w-full bg-white dark:bg-client-dark vnodesign-border dark:vnodesign-border" v-for="client, k in clients" :key="k">
+           <div class="card w-full bg-white bg-client vnodesign-border" v-for="client, k in clients" :key="k">
                 <client-thumbnail :client="client"/>
                 <client-details :client="client"/>
            </div>
