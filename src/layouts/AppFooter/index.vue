15c54b8e46fba69feb59c9d52c5c55d85d2473a1
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-08-07 17:28:26 +0700

    Update index.vue
    Change layout footer

--- src/layouts/AppFooter/index.vue
+++ src/layouts/AppFooter/index.vue
@@ -3,16 +3,9 @@
 </script>
 
 <template>
-	<footer class="flex justify-center relative py-5 bg-white bg-opacity-70 mt-20">
-			<p class="text-sm mt-5 mb-5">
-				Được tạo bằng cả <i class="fa fa-heart"></i> <span class="dot">·</span> Xây dựng và bảo trì bởi <strong><a href="https://github.com/vnodesign" target="_blank" title="VNO Design" class="text-main-color">VNO Design</a></strong>
-			</p>
+	<footer class="flex justify-center relative py-5 mt-20">
+		<p class="text-sm mt-5 mb-5">
+		   VNO DESIGN
+		</p>
 	</footer>
 </template>
-<style lang="scss">
-footer {
-	.fa-heart {
-		color: red;
-	}
-}
-</style>
