5b6534dabba833e127a0a3489ac1ecb7e63c3d74
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-08-09 14:44:49 +0700

    Update index.vue

--- src/layouts/AppFooter/index.vue
+++ src/layouts/AppFooter/index.vue
@@ -1,14 +1,7 @@
 <template>
 	<footer class="flex justify-center relative py-5 mt-20">
-		<!-- Arrow top (back to top) -->
-		<a class="center-element rounded-full mx-auto absolute -top-5 w-10 h-10 border-2 bg-white border-gray-200 dark:bg-gray-800 dark:border-gray-900" role="button" href="#intro" aria-label="Go to top">
-			<i class="fa fa-arrow-up"></i>
-		</a>
-		<!-- End arrow top -->
-		<div>
-			<p class="text-sm mt-5">
-			  © {{ year }} vnodesign
-			</p>
-		</div>
+	  <p class="text-sm mt-5">
+	        © {{ year }} VNO DESIGN
+	  </p>
 	</footer>
 </template>
