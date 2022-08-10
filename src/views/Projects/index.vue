dd8d3daea48bfd0b21d66782fd0f431dca3c373b
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-08-10 17:01:25 +0700

    Update index.vue

--- src/views/Projects/index.vue
+++ src/views/Projects/index.vue
@@ -11,7 +11,7 @@
    <section id="projects">
       <section-content title="Sản phẩm">
          <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-           <div class="card w-full bg-white bg-product vnodesign-border" v-for="project, k in projects" :key="k">
+           <div class="card w-full bg-white bg-product vnodesign-border image-full" v-for="project, k in projects" :key="k">
                 <project-thumbnail :project="project"/>
                 <project-details :project="project"/>
            </div>
