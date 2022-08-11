f5c260e2e6dd5f1bb789c703b0fb22428fc42fcb
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-08-12 01:02:56 +0700

    Update index.vue

--- src/views/Experiences/index.vue
+++ src/views/Experiences/index.vue
@@ -10,7 +10,7 @@
          <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
             <div
                class="flex bg-white bg-exp rounded-lg 2xl:rounded-xl p-3 vnodesign-border"
-               v-for="{ periode, jobtitle, company, tasks }, k in experiences"
+               v-for="{ periode, jobtitle, company, teamsize, role, time, tasks }, k in experiences"
                :key="`xp-${ k }`"
             >
                <!-- Experience details -->
@@ -25,7 +25,25 @@
                   </h2>
                   <!-- End Experience title & company -->
 
-                  <!-- Experience tasks -->
+                  <!-- Experience team size, role, time and task -->
+                  <p
+                     class="mb-3 last:mb-0 text-sm xl:text-base"
+                     v-for="teamsize, k in teamsizes" :key="`teamsize-${ k }`"
+                  >
+                     Teamsize: {{ teamsize }}
+                  </p>
+                  <p
+                     class="mb-3 last:mb-0 text-sm xl:text-base"
+                     v-for="role, k in roles" :key="`role-${ k }`"
+                  >
+                     Vị trí: {{ role }}
+                  </p>
+<p
+                     class="mb-3 last:mb-0 text-sm xl:text-base"
+                     v-for="time, k in times" :key="`time-${ k }`"
+                  >
+                     Thời gian làm việc: {{ time }}
+                  </p>
                   <p
                      class="mb-3 last:mb-0 text-sm xl:text-base"
                      v-for="task, k in tasks" :key="`task-${ k }`"
