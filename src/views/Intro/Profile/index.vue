8e2fb6796176f6fb064b7d9a670cb7f3483f9d99
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-08-12 14:28:48 +0700

    Update index.vue

--- src/views/Intro/Profile/index.vue
+++ src/views/Intro/Profile/index.vue
@@ -7,40 +7,15 @@
 </script>
 
 <template>
-   <div class="order-1">
-      <!-- Personal infos -->
-      <div class="relative flex flex-col-reverse">
-         <div class="flex items-center space-x-4">
-         <!-- Avatar -->
-         <img class="flex-none w-24 h-24 rounded-full object-cover" :alt="`${ intro.fullname } photo`" :src="`./assets/${ intro.avatar }`" loading="lazy" deasync="true">
-         <!-- End avatar -->
-         <div class="flex-auto ml-2">
-         <!-- Fullname -->
-         <h1 class="text-3xl lg:text-4xl xl:text-5xl font-bold">
-            {{ intro.fullname }}
-         </h1>
-         <!-- End fullname -->
-         <!-- Role -->
-         <p class="text-main-color font-semibold text-lg mt-0.5">
-            {{ intro.role }}
-         </p>
-         <!-- End Role -->
-         </div>
-         </div>
-      </div>
-      <!-- Action -->
-      <div class="relative mt-3">
-         <!-- Github -->
-         <a class="font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 border vnodesign-border-color !bg-transparent text-main-color gap-2 normal-case lg:gap-3 mt-2" role="button" :href="intro.links[projectLinks]" title="Github Profile" target="_blank" onClick="ga('event', 'ProjectsLink', 'view', 'projects_link');">
-             <i :class="`fa fa-${ projectLinks } mr-2`" />
-             <span>Github Profile</span>
-         </a>
-         <!-- Coverletter -->
-          <a class="font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 border bg-vnodesign vnodesign-border-color !text-white gap-2 normal-case lg:gap-3 mt-2 ml-2" role="button" id="show__modal" href="#view_coverletter" title="Thư xin việc" onClick="ga('event', 'CoverletterPopup', 'view', 'coverletter_popup');">
-             <i class="fa fa-pencil mr-2" />
-             <span>Thư xin việc</span>
-          </a>
-         <!-- End Coverletter -->
-      </div>
-   </div>
+<div class="w-full max-w-sm bg-white rounded-lg border vnodesign-border-color">
+    <div class="flex flex-col items-center pb-10">
+        <img class="mb-3 w-24 h-24 rounded-full" :alt="`${ intro.fullname } photo`" :src="`./assets/${ intro.avatar }`" loading="lazy" deasync="true">
+        <h5 class="mb-1 text-xl font-medium">{{ intro.fullname }}</h5>
+        <span class="text-sm text-main-color">{{ intro.role }}</span>
+        <div class="flex mt-4 space-x-3 md:mt-6">
+            <a :href="intro.links[projectLinks]" title="Github Profile" target="_blank" onClick="ga('event', 'ProjectsLink', 'view', 'projects_link');" class="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-main-color border vnodesign-border-color rounded-lg">Github Profile</a>
+            <a id="show__modal" href="#view_coverletter" title="Thư xin việc" onClick="ga('event', 'CoverletterPopup', 'view', 'coverletter_popup');" class="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-white bg-vnodesign rounded-lg border vnodesign-border-color">Thư xin việc</a>
+        </div>
+    </div>
+</div>
 </template>
