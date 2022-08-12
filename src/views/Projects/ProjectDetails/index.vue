3b186cf13f8d82693ac5327d22426160f0fa8475
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-08-12 14:16:41 +0700

    Update index.vue

--- src/views/Projects/ProjectDetails/index.vue
+++ src/views/Projects/ProjectDetails/index.vue
@@ -7,7 +7,7 @@
 <template>
    <div class="p-5">
     <a v-for="link, k in project.links" :href="link" :key="k" target="_blank" onClick="ga('event', 'ProjectLink', 'view', 'project_link');">
-       <h5 class="mb-2 text-2xl font-bold tracking-tight">
+       <h5 class="mb-2 text-2xl font-bold tracking-tight text-main-color">
           {{ project.title }}
        </h5>
     </a>
