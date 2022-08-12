520ac837d827aff64c8546cad061ed82554ffde7
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-08-12 13:34:07 +0700

    Update index.vue

--- src/views/Projects/ProjectDetails/index.vue
+++ src/views/Projects/ProjectDetails/index.vue
@@ -5,22 +5,17 @@
 </script>
 
 <template>
-   <div class="card-body">
-    <h2 class="card-title">
-       {{ project.title }}
-    </h2>
-    <p>
-       <a class="text-xs lg:text-base text-main-color" v-for="link, k in project.links" :href="link" :key="k" target="_blank" onClick="ga('event', 'ProjectLink', 'view', 'project_link');">
-           <strong>{{ link }}</strong>
-       </a>
+   <div class="p-5">
+    <a v-for="link, k in project.links" :href="link" :key="k" target="_blank" onClick="ga('event', 'ProjectLink', 'view', 'project_link');">
+       <h5 class="mb-2 text-2xl font-bold tracking-tight">
+          {{ project.title }}
+       </h5>
+    </a>
+    <p class="mb-3 font-normal" v-for="description, k in project.description" :key="`description-${ k }`">
+       {{ description }}
     </p>
-    <p class="leading-relaxed text-left text-sm xl:text-base" v-for="description, k in project.description" :key="`description-${ k }`">
-          {{ description }}
-    </p>
-    <div class="card-actions justify-end">
-      <a class="badge badge-outline p-3" :href="`${ project.sourceCodeUrl }`" target="_blank" onClick="ga('event', 'SourceCodeUrl', 'view', 'source_code_url');">
-          Source code
-      </a>
-    </div>
+    <a class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-vnodesign rounded-lg" :href="`${ project.sourceCodeUrl }`" target="_blank" onClick="ga('event', 'SourceCodeUrl', 'view', 'source_code_url');">
+       Source code
+    </a>
   </div>
 </template>
