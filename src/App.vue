12388d0bc952cf3bb720be3cdc6d841cfb55e740
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-08-14 02:57:13 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
    import AppContainer from './layouts/AppContainer/index.vue'
    import CoverLetter from './views/CoverLetter/index.vue'
-   // import AppFooter from './layouts/AppFooter/index.vue'
+   import Contact from './views/Contact/index.vue'
 </script>
 
 <template>
    <CoverLetter />
    <app-container />
+   <contact />
 </template>
 
 <style lang="scss">
