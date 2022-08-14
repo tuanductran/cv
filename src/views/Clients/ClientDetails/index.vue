63837d695c9c4d8af3fb5adb878d3cce9de347a9
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-08-14 13:39:39 +0700

    Update index.vue

--- src/views/Clients/ClientDetails/index.vue
+++ src/views/Clients/ClientDetails/index.vue
@@ -5,20 +5,17 @@
 </script>
 
 <template>
-   <div class="card-body">
-    <h2 class="card-title">{{ client.title }}</h2>
-    <p>
-       <a class="text-xs lg:text-base text-main-color" v-for="link, k in client.links" :href="link" :key="k" target="_blank" onClick="ga('event', 'ClientLink', 'view', 'client_link');">
-           <strong>{{ link }}</strong>
-       </a>
-    </p>
-    <p class="leading-relaxed text-left text-sm xl:text-base" v-for="description, k in client.description" :key="`description-${ k }`">
+   <div class="p-5">
+    <a v-for="link, k in client.links" :href="link" :key="k" target="_blank" onClick="ga('event', 'ClientLink', 'view', 'client_link');">
+      <h5 class="mb-2 text-2xl font-bold tracking-tight">
+          {{ client.title }}
+      </h5>
+    </a>
+    <p class="mb-3 font-normal" v-for="description, k in client.description" :key="`description-${ k }`">
           {{ description }}
     </p>
-    <div class="card-actions justify-end">
-      <div class="badge badge-outline p-3" v-for="tech, k in client.builtWidth" :key="k">
+    <div class="inline-flex items-center" v-for="tech, k in client.builtWidth" :key="k">
           {{ tech }}
-      </div>
     </div>
   </div>
 </template>
